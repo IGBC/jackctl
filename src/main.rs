f290dc74848be3f7e4363a0ede31272759e0ff3a
Katharina Fey <kookie@spacekookie.de>
2021-09-27 16:23:51 +0200

    Initialise settings tree on launch

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,36 @@ mod process_manager;
 mod settings;
 mod ui;
 
+use directories::ProjectDirs;
 use gio::prelude::*;
 use std::env::args;
 
 fn main() {
-    {
-        if gtk::init().is_err() {
-            println!("Failed to start GTK, please ensure all dependancies are installed");
-        }
+    // Load and initialise settings first
+    let dir = ProjectDirs::from("tech", "sigsegv", "jackctl").unwrap();
+    let set = settings::Settings::init(dir.config_dir());
 
-        // due to a bug this button is basically panic on demand, however it does the job.
-        ctrlc::set_handler(|| gtk::main_quit()).expect("Error setting Ctrl-C handler");
-
-        let model = model::ModelInner::new();
-
-        let proc_manager = process_manager::ProcessManager::new(model.clone());
-        let jack_controller = jack::JackController::new(model.clone());
-        let alsa_controller = mixer::MixerController::new(model.clone());
-        let (window, app) = ui::init_ui(
-            model.clone(),
-            jack_controller.clone(),
-            alsa_controller.clone(),
-        );
-        window.borrow().show();
-
-        app.run(&args().collect::<Vec<_>>());
-        proc_manager.borrow_mut().end();
+    if gtk::init().is_err() {
+        println!("Failed to start GTK, please ensure all dependancies are installed");
     }
+
+    // due to a bug this button is basically panic on demand, however it does the job.
+    ctrlc::set_handler(|| gtk::main_quit()).expect("Error setting Ctrl-C handler");
+
+    let model = model::ModelInner::new();
+
+    let proc_manager = process_manager::ProcessManager::new(model.clone());
+    let jack_controller = jack::JackController::new(model.clone());
+    let alsa_controller = mixer::MixerController::new(model.clone());
+    let (window, app) = ui::init_ui(
+        model.clone(),
+        jack_controller.clone(),
+        alsa_controller.clone(),
+    );
+    window.borrow().show();
+
+    app.run(&args().collect::<Vec<_>>());
+    proc_manager.borrow_mut().end();
 
     println!("Jackctl Exiting, Goodbye");
 }
