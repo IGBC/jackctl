f17e47878bab9d6bb36fef638a3c065b23b1979c
Katharina Fey <kookie@spacekookie.de>
2022-01-08 17:54:47 +0100

    Add contributors file included into the application binary

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use crate::ui::utils;
 use gtk::{AboutDialog, AboutDialogExt, Builder, ButtonExt, DialogExt, ModelButton, WidgetExt};
 
@@ -5,10 +7,47 @@ pub struct About {
     inner: AboutDialog,
 }
 
+const CONTRIBUTORS: &'static str = include_str!("../../contributors.toml");
+type ContributorMap = BTreeMap<String, BTreeMap<String, String>>;
+
+fn unpack_map(map: Option<&BTreeMap<String, String>>) -> Vec<String> {
+    map.map(|map| map.iter().map(|(k, v)| format!("{} <{}>", k, v)).collect())
+        .unwrap_or_default()
+}
+
 impl About {
     /// Create the main about dialog builder
     pub fn new(b: &Builder) -> Self {
         let inner: AboutDialog = utils::get_object(b, "aboutdialog");
+
+        // Parse contributors
+        match toml::from_str::<ContributorMap>(&CONTRIBUTORS) {
+            Ok(c) => {
+                let devers = unpack_map(c.get("developers"));
+                let artoids = unpack_map(c.get("artists"));
+                let transes = unpack_map(c.get("translators"));
+
+                inner.set_authors(
+                    devers
+                        .iter()
+                        .map(|s| s.as_str())
+                        .collect::<Vec<_>>()
+                        .as_slice(),
+                );
+                inner.set_artists(
+                    artoids
+                        .iter()
+                        .map(|s| s.as_str())
+                        .collect::<Vec<_>>()
+                        .as_slice(),
+                );
+                inner.set_translator_credits(Some(transes.join("\n").as_str()));
+            }
+            Err(e) => {
+                error!("Failed to parse contributors: {:?}", e)
+            }
+        }
+
         inner.set_version(Some(env!("CARGO_PKG_VERSION")));
         inner.connect_response(move |dialog, _| dialog.hide());
         Self { inner }
