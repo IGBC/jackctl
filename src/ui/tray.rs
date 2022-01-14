3f67864952f166be9c543a664f1cd6811369a28c
SEGFAULT <fakespamtrap@gmail.com>
2022-01-14 14:49:01 +0100

    Fixed tray icon (dynamically loaded from /tmp at start)

--- src/ui/tray.rs
+++ src/ui/tray.rs
@@ -1,21 +1,38 @@
+use gio::ResourceLookupFlags;
 use gtk::prelude::*;
 use gtk::Window;
 use libappindicator::{AppIndicator, AppIndicatorStatus};
-use std::path::Path;
+
+use std::env::temp_dir;
+use std::fs::{remove_file, File};
+use std::io::prelude::*;
+use std::path::PathBuf;
 
 use crate::model::events::UiEvent;
 use crate::ui::UiRuntime;
 
 pub struct TrayState {
     indicator: AppIndicator,
+    icon_file: PathBuf,
 }
 
 impl TrayState {
     pub(super) fn new(rt: UiRuntime, window: Window) -> Self {
-        let icon_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("src");
+        let icon_path: PathBuf = temp_dir();
+        info!("Indicator Icon Path = {}", icon_path.to_str().unwrap());
+
+        let icon_file = icon_path.join("jackctl-symbolic.svg");
+        let mut file = File::create(icon_file.clone().into_os_string()).unwrap();
+        let icon = gio::resources_lookup_data(
+            "/net/jackctl/Jackctl/icons/jackctl-symbolic.svg",
+            ResourceLookupFlags::NONE,
+        )
+        .unwrap();
+        file.write_all(&icon);
 
         let mut indicator = AppIndicator::new("jackctl", "");
         indicator.set_status(AppIndicatorStatus::Active);
+        indicator.set_icon_theme_path(icon_path.to_str().unwrap());
         indicator.set_icon("jackctl-symbolic");
         let mut m = gtk::Menu::new();
         let mi = gtk::CheckMenuItem::with_label("exit");
@@ -29,6 +46,15 @@ impl TrayState {
         indicator.set_menu(&mut m);
         m.show_all();
 
-        Self { indicator }
+        Self {
+            indicator,
+            icon_file,
+        }
+    }
+}
+
+impl Drop for TrayState {
+    fn drop(&mut self) {
+        let _ = remove_file(self.icon_file.clone().into_os_string());
     }
 }
